d5735a5d9a157d4152e68aa45083f8af6190ea55
Kanat Asanbekov <kanatqa@gmail.com>
2020-02-22 09:34:45 -0800

    Updates

---
+++ precache-manifest.a8f194b8b1a781496aa4b79eb8059b6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b4f71a925d89dcf0a4fbee72e473fd8",
+    "url": "/cra1/index.html"
+  },
+  {
+    "revision": "729d2bf007f54160d065",
+    "url": "/cra1/static/css/main.2070e803.chunk.css"
+  },
+  {
+    "revision": "bac717b3829ef4ba86ef",
+    "url": "/cra1/static/js/2.c76f94f9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cra1/static/js/2.c76f94f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "729d2bf007f54160d065",
+    "url": "/cra1/static/js/main.d6a495af.chunk.js"
+  },
+  {
+    "revision": "358fadb87d4716f3bbce",
+    "url": "/cra1/static/js/runtime-main.04104b05.js"
+  }
+]);
